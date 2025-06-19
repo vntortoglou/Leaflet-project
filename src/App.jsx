f831a542b92351cf357b59b6186e0418c6071b46
Vangelis <vntortoglou@gmail.com>
2025-06-20 02:23:08 +0300

    Get default position with geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,60 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState, useEffect } from "react";
 
 //Import Components here
 import SearchBar from "./SearchBar";
 
-
-
-
-
 const App = () => {
+  // Getting the Current position to set as Map Default
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition(pos.coords);
+      },
+      (error) => {
+        console.error("Geolocation error:", error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+      }
+    );
+  }, []);
+
   return (
     <div className="flex h-screen w-screen">
-
-
       {/* Sidebar */}
       <div className="w-1/6 bg-green-100 border-r border-black p-4">
         <h2 className="text-xl font-bold mb-4">SideBar</h2>
-        {/* Add your components here */}
-        <SearchBar></SearchBar>
-
+        <SearchBar />
       </div>
 
       {/* Map Area */}
       <div className="w-5/6">
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={true}
-          className="h-full w-full"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>London calling!</Popup>
-          </Marker>
-        </MapContainer>
+        {/*Ternary operator to load the map only when there is a position*/}
+        {position ? (
+          <MapContainer
+            center={[position.latitude, position.longitude]}
+            zoom={13}
+            scrollWheelZoom={true}
+            className="h-full w-full"
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[position.latitude, position.longitude]}>
+              <Popup>Your Location</Popup>
+            </Marker>
+          </MapContainer>
+        ) : (
+          <p className="text-center mt-10">Loading map...</p>
+        )}
       </div>
     </div>
   );
