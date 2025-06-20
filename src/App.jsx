a3290251006eb424cc9500183df9fc4c89417f18
Vangelis <vntortoglou@gmail.com>
2025-06-20 15:48:16 +0300

    Added Axios Libraty

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { useState, useEffect } from "react";
 
 //Import Components here
 import SearchBar from "./SearchBar";
 
 const App = () => {
-  // Getting the Current position to set as Map Default
+
+
+  // START : Getting the Current position to set as Map Default
   const [position, setPosition] = useState(null);
 
   useEffect(() => {
@@ -25,13 +27,27 @@ const App = () => {
       }
     );
   }, []);
+    // END : Getting the Current position to set as Map Default
+
+    function FlyToLocation({ position }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (position) {
+      map.flyTo([position.latitude, position.longitude], 13);
+    }
+  }, [position]);
+
+  return null;
+}
+    
 
   return (
     <div className="flex h-screen w-screen">
       {/* Sidebar */}
       <div className="w-1/6 bg-green-100 border-r border-black p-4">
         <h2 className="text-xl font-bold mb-4">SideBar</h2>
-        <SearchBar />
+        <SearchBar setPosition={setPosition} />
       </div>
 
       {/* Map Area */}
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,9 +1,26 @@
-export default function SearchBar() {
+import { useMap } from "react-leaflet";
+import { useEffect } from "react";
+
+export default function SearchBar({ setPosition }) {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    
+    try {
+      const response = await fetch()
+    }
+
+
+  };
+
   return (
-    <form className="flex flex-col gap-4 p-4 border border-black bg-white rounded-md shadow-md">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4 border border-black bg-white rounded-md shadow-md">
       <h2 className="text-lg font-bold text-black">Search City</h2>
-      
+
       <input
+        name="city"
         type="text"
         placeholder="Enter city name..."
         className="px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
